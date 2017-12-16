24a559ef5e0805c0552107dfd78b871cf176e078
Denis Solovyov <solovyov.denis@gmail.com>
2017-12-17 00:17:13 +0200

    added api view file

---
+++ views/site/api-test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denis
+ * Date: 11/12/2017
+ * Time: 16:08
+ */
+
+echo __FILE__ . PHP_EOL;
+
+echo "<pre>" . print_r($response, true) . "</pre>" . PHP_EOL;
